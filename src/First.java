936533415d67a248df78d3dc7ebbfdf0a8ef237d
7000014495 <muttu068@gmail.com>
2017-10-14 14:36:57 +0530

    Lets see again

--- src/First.java
+++ src/First.java
@@ -2,6 +2,5 @@ public class First{
     public static void main(String[] args){
         System.out.println("Hello Parvez");
 	System.out.println("Hi, Commit is made");
-	System.out.println("Hi not getting");
     }
 }
